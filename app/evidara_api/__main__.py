0a58185f58d6f0add2aab7228b7fdba169e147f1
Brett Smith <brett.a.smi@gmail.com>
2020-03-06 18:21:06 -0800

    update localhost to explicit ip to address potential collaborator bug

--- app/evidara_api/__main__.py
+++ app/evidara_api/__main__.py
@@ -9,7 +9,7 @@ from flask import g, render_template
 from evidara_api import encoder
 from evidara_api.spoke_constants import BIOLINK_SPOKE_NODE_MAPPINGS
 
-uri = "bolt://localhost:7687"
+uri = "bolt://0.0.0.0:7687"
 neo4j_user = os.getenv("NEO4J_SPOKE_USER")
 neo4j_pass = os.getenv("NEO4J_SPOKE_PASSWORD")
 driver = neo4j.GraphDatabase.driver(uri, auth=("neo4j", neo4j_pass))
