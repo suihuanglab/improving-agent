d80f71787b09c28f38eafacf425b4ad57e797ca9
Brett Smith <brett.a.smi@gmail.com>
2020-04-26 22:21:27 -0700

    fix missing headers in result table

--- app/evidara_api/static/evidara.js
+++ app/evidara_api/static/evidara.js
@@ -476,9 +476,9 @@ const createAttributeHeaderRows = function(
 ) {
   const attributeHeaderRow = table.insertRow();
   attributeHeaderRow.setAttribute("class", row_class);
-  for (i = 1; i <= queryLength + 1; i++) {
+  for (i = 0; i <= queryLength + 1; i++) {
     let cellClass;
-    if (i % 2 == 0) {
+    if (i % 2 == 1) {
       // insert an edge column if even
       cellClass = { class: row_class + "-edge" + " cell-sub-head" };
     } else {
