c666052be34c83a3a69ef2cd8f01a147a3024cf4
Brett Smith <bsmith@systemsbiology.org>
2021-01-19 13:45:02 -0800

    TRAPI 1.0 part 4 - predicates endpoint (#24)
    * add predicates endpoint
    
    * fix biolink spoke edge mapping
    
    * TRAPI 1.0 part 5 - error handling  (#25)
    
    * better error handling; fix result truncating bug
    
    * minor changes to openapi.yaml
    
    * add edge normalization (#27)

--- app/improving_agent/controllers/predicates_controller.py
+++ app/improving_agent/controllers/predicates_controller.py
@@ -1,7 +1,17 @@
+from collections import defaultdict
+
 import connexion
 import six
 
 from improving_agent import util
+from improving_agent.src.spoke_biolink_constants import PREDICATES
+
+TRAPI_PREDICATES = defaultdict(dict)
+for biolink_subject, biolink_objects in PREDICATES.items():
+    for biolink_object, biolink_predicates in biolink_objects.items():
+        TRAPI_PREDICATES[biolink_subject][biolink_object] = list(biolink_predicates.keys())
+
+TRAPI_PREDICATES = {k: TRAPI_PREDICATES[k] for k in sorted(TRAPI_PREDICATES)}
 
 
 def predicates_get():  # noqa: E501
@@ -12,4 +22,4 @@ def predicates_get():  # noqa: E501
 
     :rtype: Dict[str, Dict[str, List[str]]]
     """
-    return 'do some magic!'
+    return TRAPI_PREDICATES
